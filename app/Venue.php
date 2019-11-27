dc52502b6fd195408a6f775fb9bec694ccb8914b
Gintaras Arštikaitis <garstikaitis@gmail.com>
2019-11-27 20:36:11 +0100

    add slugsto venues

--- app/Venue.php
+++ app/Venue.php
@@ -3,11 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Venue extends Model
 {
+    use HasSlug;
+
     protected $fillable = ['name'];
     public function manager() {
         return $this->belongsTo('\App\User');
+    }
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
     }
 }
